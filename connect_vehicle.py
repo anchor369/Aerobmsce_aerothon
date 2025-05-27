078752e71553bb7d1d01adb972d93037ab10ac3f
anchor369 <89133707+anchor369@users.noreply.github.com>
2025-05-27 06:49:20 +0530

    Update connect_vehicle.py

--- connect_vehicle.py
+++ connect_vehicle.py
@@ -1,5 +1,9 @@
 from dronekit import connect
 
 def connect_drone():
-    print("Connecting to vehicle on localhost:14550 (SITL/Mission Planner)...")
-    return connect('127.0.0.1:14550', wait_ready=True)
+	print("🔌 Connecting to SITL on UDP port 14550...")
+
+	# Use TCP connection for ArduPilot SITL
+	vehicle = connect('udp:127.0.0.1:14550', wait_ready=True)
+	print("✅ Connected to simulated drone!")
+	return vehicle
